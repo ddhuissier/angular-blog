2e4536eb8ab6482a1c287dea7a2f8d061257318d
ddhuissier <85339072+ddhuissier@users.noreply.github.com>
2023-09-17 17:34:36 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/home/home.component').then(mod => mod.HomeComponent)
+    loadComponent: () => import('./components/home/home.component').then(mod => mod.HomeComponent),
+    title: "Welcome !"
   },
+  /* {
+    path: 'about',
+    loadComponent: () => import('./components/about/about.component').then(mod => mod.AboutComponent),
+    title: "About Me"
+  }, */
   {
     path: '**',
     loadComponent: () => import('./components/not-found/not-found.component').then(mod => mod.NotFoundComponent)
